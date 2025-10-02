db4b636babc17938eb1da3aff3812bfc29a80d1e
itsm-a-dev <max.arab117@gmail.com>
2025-10-01 23:21:34 -0400

    Update db.py

--- db.py
+++ db.py
@@ -1,11 +1,18 @@
-import os, psycopg2
+import os
+import psycopg2
 from urllib.parse import urlparse
 
 db_url = os.getenv("DATABASE_URL")
+if not db_url:
+    raise RuntimeError("DATABASE_URL not set")
+
 url = urlparse(db_url)
 conn = psycopg2.connect(
-    dbname=url.path[1:], user=url.username,
-    password=url.password, host=url.hostname, port=url.port
+    dbname=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
 )
 conn.autocommit = False
 
@@ -13,8 +20,10 @@ def exec_safe(sql, params=None, fetch="none"):
     try:
         with conn.cursor() as cur:
             cur.execute(sql, params or [])
-            if fetch == "one": return cur.fetchone()
-            if fetch == "all": return cur.fetchall()
+            if fetch == "one":
+                return cur.fetchone()
+            elif fetch == "all":
+                return cur.fetchall()
             return None
     except psycopg2.Error:
         conn.rollback()
